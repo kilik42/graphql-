f30cc9a24162b65ce09f3eefaf051ca961ea26fe
kilik42 <marvinevins@yahoo.com>
2022-06-06 12:55:38 -0500

    working on schema

--- server/index.js
+++ server/index.js
@@ -1,13 +1,16 @@
+
 const express = require('express');
+const colors = require('colors');
+const cors = require('cors');
 require('dotenv').config();
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
+// const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
-const schema = require('./schema/schema')
-const {graphqlHTTP} = require('express-graphql');
 const app = express();
 
-
-
-app.use('/graphql', graphqlHTTP({
+app.use('/graphql', 
+   graphqlHTTP({
     schema, 
     graphiql: process.env.NODE_ENV === 'development'
 }))
---
+++ server/schema/schema.js
@@ -0,0 +1,49 @@
+const {projects, clients} = require('../sampleData.js');
+
+// const graphql = require('graphql');
+
+const {
+    GraphQLObjectType,
+    GraphQLID,
+    GraphQLString,
+    GraphQLSchema,
+    GraphQLList,
+    GraphQLNonNull,
+    GraphQLEnumType,
+  } = require('graphql');
+
+//client type
+
+const ClientType = new GraphQLObjectType({
+        name : 'Client',
+        fields : () =>({
+             id: {type: GraphQLID} ,
+             name: {type: GraphQLString},
+              email: {type: GraphQLString},
+              phone: {type: GraphQLString} 
+        })
+});
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        clients: {
+            type: new GraphQLList(ClientType),
+            resolve(parent,args){
+                return clients
+            }
+        },
+        client: {
+            type: ClientType,
+            args: { id: {type: GraphQLID} },
+            resolve(parent, args){
+                return clients.find(client => client.id === args.id);
+
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query:RootQuery,
+
+})
