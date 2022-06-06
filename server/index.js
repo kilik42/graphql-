2091b990df2056673f5a8d41b3eb80aada6ee2ea
kilik42 <marvinevins@yahoo.com>
2022-06-06 11:28:51 -0500

    first commit

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const schema = require('./schema/schema')
+const {graphqlHTTP} = require('express-graphql');
+const app = express();
+
+
+
+app.use('/graphql', graphqlHTTP({
+    schema, 
+    graphiql: process.env.NODE_ENV === 'development'
+}))
+
+app.listen(port, console.log(`server running on port ${port}`));
